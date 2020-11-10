49ca02bfcdad23abc74a22b12563cf5c89a3186b
jsp <jonsp14@gmail.com>
2020-11-10 15:06:18 -0800

    convert main component to class, add character json object

--- adventure-time-clicker/src/components/Image.js
+++ adventure-time-clicker/src/components/Image.js
@@ -15,16 +15,43 @@ import prismo from "../assets/prismo1.jpg";
 var array = [finn, jake, marceline, pbg, bmo, iceking, flame, lsp, lemongrab, gunther, peppermint, prismo];
 function shuffle() {
     for (let i = 0; i < array.length; i++) {
-      var x = Math.floor(Math.random() * i);
-      var y = array[i];
-      array[i] = array[x];
-      array[x] = y;
+        var x = Math.floor(Math.random() * i);
+        var y = array[i];
+        array[i] = array[x];
+        array[x] = y;
     }
 }
 
-function Image(props){
-    // shuffle();
-    return <img className="img" src={array[props.index]} alt="Adventure Time Character"></img>;
+let calculate = calc => {
+    console.log("calc", calc);
+    console.log("WORKING");
+    console.log(this);
+    shuffle();
+}
+
+function Image(props) {
+    shuffle();
+
+    return <div className="container text-center" >
+        <div className="row pb-3 pt-5">
+            <div className="col character" onClick={calculate} data-value={array[0]}><img className="img" src={array[0]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[1]}><img className="img" src={array[1]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[2]}><img className="img" src={array[2]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[3]}><img className="img" src={array[3]} alt="Adventure Time Character"></img></div>
+        </div>
+        <div className="row pb-3">
+            <div className="col character" onClick={calculate} data-value={array[4]}><img className="img" src={array[4]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[5]}><img className="img" src={array[5]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[6]}><img className="img" src={array[6]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[7]}><img className="img" src={array[7]} alt="Adventure Time Character"></img></div>
+        </div>
+        <div className="row pb-3">
+            <div className="col character" onClick={calculate} data-value={array[8]}><img className="img" src={array[8]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[9]}><img className="img" src={array[9]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[10]}><img className="img" src={array[10]} alt="Adventure Time Character"></img></div>
+            <div className="col character" onClick={calculate} data-value={array[11]}><img className="img" src={array[11]} alt="Adventure Time Character"></img></div>
+        </div>
+    </div>;
 }
 
 export default Image;
--- adventure-time-clicker/src/components/Main.js
+++ adventure-time-clicker/src/components/Main.js
@@ -1,68 +1,53 @@
 import React from "react";
-import Image from "./Image";
+import Image from "./Image"
 import "../styles/Main.css";
 
-
-import finn from "../assets/finn.png";
-import jake from "../assets/jake1.jpg";
-import marceline from "../assets/marceline.jpg";
-import pbg from "../assets/princessbub.png";
-import bmo from "../assets/bmo1.jpg";
-import iceking from "../assets/iceking.jpg";
-import flame from "../assets/flameprincess.png";
-import lsp from "../assets/lsp1.jpg";
-import lemongrab from "../assets/lemongrab1.png";
-import gunther from "../assets/gunther1.jpg";
-import peppermint from "../assets/peppermintbutler1.jpg";
-import prismo from "../assets/prismo1.jpg";
+import characters from "../assets/adventure.json"
 
 // var container = document.getElementsByClassName("container-fluid");
-var array = [finn, jake, marceline, pbg, bmo, iceking, flame, lsp, lemongrab, gunther, peppermint, prismo];
 
-// shuffle();
 
-function shuffle() {
-  for (let i = 0; i < array.length; i++) {
-    var x = Math.floor(Math.random() * i);
-    var y = array[i];
-    array[i] = array[x];
-    array[x] = y;
+class Main extends Component {
+
+  state = {
+    characters: characters,
+    correct: 0,
+    selected: [],
 
   }
-}
 
-function calculate(){
-  console.log("WORKING");
-  shuffle();
-}
+  handleClick = id => {
 
+    const name = event.target.name;
+  }
 
+  // shuffle();
 
-function Main() {
-  return (
-    <div className="wrapper">
-      <div className="container text-center" >
-        <div className="row pb-3 pt-5">
-          <div className="col character" onClick={calculate} data-value={array[0]}><Image index={0} /></div>
-          <div className="col character" onClick={calculate} data-value={array[1]}><Image index={1} /></div>
-          <div className="col character" onClick={calculate} data-value={array[2]}><Image index={2} /></div>
-          <div className="col character" onClick={calculate} data-value={array[3]}><Image index={3} /></div>
-        </div>
-        <div className="row pb-3">
-          <div className="col character" onClick={calculate} data-value={array[4]}><Image index={4} /></div>
-          <div className="col character" onClick={calculate} data-value={array[5]}><Image index={5} /></div>
-          <div className="col character" onClick={calculate} data-value={array[6]}><Image index={6} /></div>
-          <div className="col character" onClick={calculate} data-value={array[7]}><Image index={7} /></div>
-        </div>
-        <div className="row pb-3">
-          <div className="col character" onClick={calculate} data-value={array[8]}><Image index={8} /></div>
-          <div className="col character" onClick={calculate} data-value={array[9]}><Image index={9} /></div>
-          <div className="col character" onClick={calculate} data-value={array[10]}><Image index={10} /></div>
-          <div className="col character" onClick={calculate} data-value={array[11]}><Image index={11} /></div>
-        </div>
+  shuffle = () => {
+    for (let i = 0; i < array.length; i++) {
+      var x = Math.floor(Math.random() * i);
+      var y = array[i];
+      array[i] = array[x];
+      array[x] = y;
+    }
+    console.log(array);
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        {this.state.characters.map(characters => (
+          <Image
+            handleClick={this.handleClick}
+            id={characters.id}
+            key={characters.id}
+            name={characters.name}
+            image={characters.image}
+          />
+        ))}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Main;

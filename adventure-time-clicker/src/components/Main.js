bf021aad7decc9dab3a46cc5461f7ecb0fbb4e24
jsp <jonsp14@gmail.com>
2020-11-09 19:03:57 -0800

    add data-values and onclick events for each image div

--- adventure-time-clicker/src/components/Main.js
+++ adventure-time-clicker/src/components/Main.js
@@ -27,34 +27,39 @@ function shuffle() {
 
   }
   console.log(array);
-  for (let i = 0; i < array.length; i++) {
-    let content = <img src={array[i]} alt="Adventure Time Character"></img>
-    // container.innerHTML(content);
-  }
+  // for (let i = 0; i < array.length; i++) {
+  //   // let content = <img src={array[i]} alt="Adventure Time Character"></img>
+  //   // container.innerHTML(content);
+  // }
 
 }
+
+function calculate(){
+  console.log("WORKING");
+}
+
 
 function Main() {
   return (
     <div className="wrapper">
       <div className="container text-center" >
         <div className="row pb-3 pt-5">
-          <div className="col character"><img className="img" src={array[0]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[1]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[2]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[3]} alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[0]}><img className="img" src={array[0]} alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[1]}><img className="img" src={array[1]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[2]}><img className="img" src={array[2]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[3]}><img className="img" src={array[3]}  alt="Adventure Time Character"></img></div>
         </div>
         <div className="row pb-3">
-          <div className="col character"><img className="img" src={array[4]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[5]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[6]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[7]} alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[4]}><img className="img" src={array[4]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[5]}><img className="img" src={array[5]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[6]}><img className="img" src={array[6]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[7]}><img className="img" src={array[7]}  alt="Adventure Time Character"></img></div>
         </div>
         <div className="row pb-3">
-          <div className="col character"><img className="img" src={array[8]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[9]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[10]} alt="Adventure Time Character"></img></div>
-          <div className="col character"><img className="img" src={array[11]} alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[8]}><img className="img" src={array[8]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[9]}><img className="img" src={array[9]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[10]}><img className="img" src={array[10]}  alt="Adventure Time Character"></img></div>
+          <div className="col character" onClick={calculate()} data-value={array[11]}><img className="img" src={array[11]}  alt="Adventure Time Character"></img></div>
         </div>
       </div>
     </div>
